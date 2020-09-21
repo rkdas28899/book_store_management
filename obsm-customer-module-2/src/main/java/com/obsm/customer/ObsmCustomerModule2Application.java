e0ec6f53ab4e1b487ad3a65e9edfde521610997f
Revanth <Revanth@Revanth>
2020-09-21 19:10:14 +0530

    Implemented CRUD Operations for Customer Module

---
+++ obsm-customer-module-2/src/main/java/com/obsm/customer/ObsmCustomerModule2Application.java
@@ -0,0 +1,13 @@
+package com.obsm.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsmCustomerModule2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsmCustomerModule2Application.class, args);
+	}
+
+}
---
+++ obsm-customer-module-2/src/test/java/com/obsm/ObsmCustomerModule2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsmCustomerModule2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
